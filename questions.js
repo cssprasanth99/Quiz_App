59bf54e91d60951a8db762e35a467c5a02acd364
SAI SATYA PRASANTH CHAMUTURI <135578098+cssprasanth99@users.noreply.github.com>
2023-06-25 18:07:31 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,73 @@
+//Creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question:
+      "It takes 6 hours for three pipes, X, Y and Z to fill a tank. When the three worked together for 2 hours, Z was closed and, X and Y filled the remaining tank in 7 hours. How many hours would it take Z alone to fill the tank?",
+    answer: "14 HRS",
+    options: ["15 HRS", "23 HRS", "14 HRS", "20 HRS"],
+  },
+  {
+    numb: 2,
+    question:
+      "It takes two pipes A and B, running together, to fill a tank in 6 minutes. It takes A 5 minutes less than B to fill the tank, then what will be the time taken by B alone to fill the tank?",
+    answer: "15 minutes",
+    options: ["10 minutes", "15 minutes", "20 minutes", "25 minutes"],
+  },
+  {
+    numb: 3,
+    question:
+      "If two pipes can fill a tank in 24 and 20 minutes respectively and another pipe can empty 3 gallons of water per minute from that tank. When all the three pipes are working together, it takes 15 minutes to fill the tank. What is the capacity of the tank? ",
+    answer: "100 gallons",
+    options: ["100 gallons", "150 gallons", "125 gallons", "130 gallons"],
+  },
+  {
+    numb: 4,
+    question:
+      "It takes 20 minutes for pipe A to fill the tank completely and it takes 30  minutes for pipe B to fill the tank completely. If both the inlets are opened together, then how much time will be taken to fill the tank completely? ",
+    answer: "12 minutes",
+    options: ["15 minutes", "12 minutes", "11 minutes", "10 minutes"],
+  },
+  {
+    numb: 5,
+    question:
+      "Pipe A can fill the tank 3 times faster in comparison to pipe B. It takes 36 minutes for pipe A and B to fill the tank together. How much time will pipe B alone take to fill the tank?",
+    answer: "144 minutes",
+    options: ["100 minutes", "124 minutes", "134 minutes", "144 minutes"],
+  },
+  {
+    numb: 6,
+    question:
+      "Two pipes can fill a tank in 20 and 24 minutes respectively and a waste pipe can empty 3 gallons per minute. All the three pipes working together can fill the tank in 15 minutes. The capacity of the tank is:",
+    answer: "120 gallons",
+    options: ["60 gallons", "100 gallons", "120 gallons", "180 gallons"],
+  },
+  {
+    numb: 7,
+    question:
+      "A tank is filled in 5 hours by three pipes A, B and C. The pipe C is twice as fast as B and B is twice as fast as A. How much time will pipe A alone take to fill the tank?",
+    answer: "35 hours",
+    options: ["20 hours", "25 hours", "35 hours", "Cannot be determined"],
+  },
+  {
+    numb: 8,
+    question:
+      "Two pipes A and B together can fill a cistern in 4 hours. Had they been opened separately, then B would have taken 6 hours more than A to fill the cistern. How much time will be taken by A to fill the cistern separately?",
+    answer: "1 hour",
+    options: ["1 hour", "25 hours", "2 hours", "8 hours"],
+  },
+  {
+    numb: 9,
+    question:
+      "Two pipes A and B can fill a tank in 20 and 30 minutes respectively. If both the pipes are used together, then how long will it take to fill the tank?",
+    answer: "12 min",
+    options: ["12 min", "15 min", "25 min", "50 min"],
+  },
+  {
+    numb: 10,
+    question:
+      "One pipe can fill a tank three times as fast as another pipe. If together the two pipes can fill the tank in 36 minutes, then the slower pipe alone will be able to fill the tank in:",
+    answer: "144 min.",
+    options: ["81 min.", "108 min.", "144 min.", "192 min"],
+  },
+];
---
+++ script.js
@@ -0,0 +1,197 @@
+"strict";
+//getting all required elements
+const input_btn = document.querySelector(".input_btn");
+const enter_btn = document.querySelector(".enter_btn");
+const pipes_cisterns = document.querySelector(".pipes_cisterns");
+const probability = document.querySelector(".probability");
+const problemsonages = document.querySelector(".problemsonages");
+const profit_loss = document.querySelector(".profit_loss");
+const question_page = document.querySelector(".question_page");
+const Userdetails = document.querySelector(".Userdetails");
+const option_list = document.querySelector(".option_list");
+const timeCount = document.querySelector(".timer");
+
+//if category button is click
+pipes_cisterns.onclick = () => {
+  question_page.classList.add("activequestion"); // show the question page
+  Userdetails.classList.add("inactivedisplay"); // hide main page
+  showQuestions(0);
+  queCounter(1);
+  startTimer(15);
+};
+probability.onclick = () => {
+  question_page.classList.add("activequestion");
+  Userdetails.classList.add("inactivedisplay");
+};
+problemsonages.onclick = () => {
+  question_page.classList.add("activequestion");
+  Userdetails.classList.add("inactivedisplay");
+};
+profit_loss.onclick = () => {
+  question_page.classList.add("activequestion");
+  Userdetails.classList.add("inactivedisplay");
+};
+let que_count = 0;
+let que_numb = 1;
+let counter;
+let timeValue = 15;
+let userScore = 0;
+
+const next_btn = document.querySelector(".next_btn");
+const quiz_result = document.querySelector(".quiz_result");
+const start_again = document.querySelector(".start_again");
+const goto_home = document.querySelector(".goto_home");
+
+start_again.onclick = () => {
+  Userdetails.classList.add("inactivedisplay");
+  quiz_result.classList.remove("activequizpage");
+  question_page.classList.add("activequestion");
+  let que_count = 0;
+  let que_numb = 1;
+  let counter;
+  let timeValue = 15;
+  let userScore = 0;
+  showQuestions(que_count);
+  queCounter(que_numb);
+  clearInterval(counter);
+  startTimer(timeValue);
+  next_btn.style.display = "none";
+};
+
+goto_home.onclick = () => {
+  window.location.reload();
+};
+// If next button is clicked
+next_btn.onclick = () => {
+  if (que_count < questions.length - 1) {
+    que_count++;
+    que_numb++;
+    clearOption();
+    showQuestions(que_count);
+    queCounter(que_numb);
+    clearInterval(counter);
+    startTimer(timeValue);
+    next_btn.style.display = "none";
+  } else {
+    console.log("Questions completed");
+    showResultBox();
+  }
+};
+function clearOption() {
+  let option_list = document.querySelectorAll(".option_list");
+  for (let i = 0; i < 4; i++) {
+    option_list.children[i].classList.remove("correct");
+    option_list.children[i].classList.remove("incorrect");
+  }
+}
+//getting questions and options from array
+function showQuestions(index) {
+  const que_text = document.querySelector(".question");
+
+  let que_tag =
+    "<span>" +
+    questions[index].numb +
+    "." +
+    questions[index].question +
+    "</span>";
+  for (let i = 0; i < 4; i++) {
+    option_list.children[i].innerHTML = questions[index].options[i];
+  }
+  // let option_tag = document.querySelector("");
+  // '<button class="option"><span>' +
+  // questions[index].options[0] +
+  // "</span></button>" +
+  // '<button class="option"><span>' +
+  // questions[index].options[1] +
+  // "</span><button>" +
+  // '<button class="option"><span>' +
+  // questions[index].options[2] +
+  // "</span></button>" +
+  // '<button class="option"><span>' +
+  // questions[index].options[3] +
+  // "</span></button>";
+  que_text.innerHTML = que_tag;
+  // option_list.innerHTML = option_tag;
+  const option = document.querySelectorAll(".option");
+  for (let i = 0; i < option.length; i++) {
+    option[i].setAttribute("onclick", "optionSelected(this)");
+  }
+}
+function optionSelected(answer) {
+  clearInterval(counter);
+  let userAns = answer.textContent;
+  let correctAns = questions[que_count].answer;
+  let allOptions = option_list.children.length;
+
+  if (userAns == correctAns) {
+    userScore += 1;
+    console.log(userScore);
+    answer.classList.add("correct");
+    console.log("Answer is Correct");
+  } else {
+    answer.classList.add("incorrect");
+    console.log("Answer is Wrong");
+    //if answer is incorrect then automatically select correct answer
+    for (let i = 0; i < allOptions; i++) {
+      if (option_list.children[i].textContent == correctAns) {
+        option_list.children[i].setAttribute("class", "option correct");
+      }
+    }
+  }
+
+  //oncer user selected disabled all options
+  for (let i = 0; i < allOptions; i++) {
+    option_list.children[i].classList.add("disabled");
+  }
+  next_btn.style.display = "block";
+}
+
+function showResultBox() {
+  question_page.classList.remove("activequestion"); // show the question page
+  Userdetails.classList.add("inactivedisplay"); // hide main page
+  quiz_result.classList.add("activequizpage"); // show the quiz result page
+  const correct_scoreText = document.querySelector(".correct_result");
+  let correct_scoreTag = "<span><p>Correct:" + userScore + "</p></span>";
+  correct_scoreText.innerHTML = correct_scoreTag;
+  const wrong_scoreText = document.querySelector(".wrong_result");
+  let wrong_scoreTag = "<span><p>Wrong:" + (10 - userScore) + "</p></span>";
+  wrong_scoreText.innerHTML = wrong_scoreTag;
+  const percentage_scoreText = document.querySelector(".percentage");
+  let percentage_scoreTag =
+    "<span><p>Percentage:" + userScore * 10 + "%</p></span>";
+  percentage_scoreText.innerHTML = percentage_scoreTag;
+}
+
+function startTimer(time) {
+  counter = setInterval(timer, 1000);
+  function timer() {
+    timeCount.textContent = time;
+    time--;
+    if (time < 9) {
+      let addZero = timeCount.textContent;
+      timeCount.textContent = "0" + addZero;
+    }
+    if (time < 0) {
+      clearInterval(counter);
+      timeCount.textContent = "00";
+
+      let correctAns = questions[que_count].answer;
+      let allOptions = option_list.children.length;
+      for (let i = 0; i < allOptions; i++) {
+        if (option_list.children[i].textContent == correctAns) {
+          option_list.children[i].setAttribute("class", "option correct");
+        }
+      }
+      for (let i = 0; i < allOptions; i++) {
+        option_list.children[i].classList.add("disabled");
+      }
+      next_btn.style.display = "block";
+    }
+  }
+}
+
+function queCounter(index) {
+  const top_ques_counter = document.querySelector(".question_no");
+  let question_noTag = "<p>" + index + "/10</p>";
+  top_ques_counter.innerHTML = question_noTag;
+}
